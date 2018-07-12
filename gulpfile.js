917df49b2c731c283548d65f6c27315ca98bbd8d
allen391 <chentinghan@outlook.com>
2018-07-12 21:24:05 +1000

    add some screenshot

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp')
 var postcss = require('gulp-postcss')
 var px2rem = require('postcss-px2rem')
+var autoprefixer = require('autoprefixer')
+var cssnano = require('cssnano')
 
 gulp.task('default', function(){
   var processors = [
-    px2rem({remUnit: 75})
+    px2rem({remUnit: 75}),
+    autoprefixer(),
+    cssnano()
   ]
   return gulp.src('./src/css/mobile.css')
             .pipe(postcss(processors))
